4c81c074a0289dbc83589b9464b1aaea44ae861b
Ștefan Cadar <65918883+StefanFaceCod2903@users.noreply.github.com>
2022-11-06 02:21:32 +0200

    Avem background!

--- website/auth.py
+++ website/auth.py
@@ -8,21 +8,23 @@ from flask_login import login_user, login_required, logout_user, current_user, l
 auth = Blueprint('auth', __name__)
 
 
-# @auth.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         user = User.query.filter_by(email=email).first()
-#         if user:
-#             if check_password_hash(user.password, password):
-#                 flash('Logged in successfully!', category='success')
-#                 login_user(user, remember=True)
-#                 return redirect(url_for('views.home'))
-#             else:
-#                 flash('Incorrect password, try again.', category='error')
-#         else:
-#             flash('Email does not exist.', category='error')
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
 
-#     return render_template("login.html", user=current_user)
+    return render_template("signIn.html", user=current_user)
 
 
 # @auth.route('/logout')
